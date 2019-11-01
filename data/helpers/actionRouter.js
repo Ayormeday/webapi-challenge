55ff79b1e81655dc46c2dc54710abdbb288f6710
Ayomide Jones <ayormeday@gmail.com>
2019-11-01 12:10:03 +0100

    inser middle ware in action resource

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,26 +1,87 @@
-const express = require('express');
-const Actions = require('./actionModel.js');
-
-
+const express = require("express");
+const Actions = require("./actionModel.js");
+const Projects = require("./projectModel.js");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Actions.get()
-      .then(action => {
-        res.status(200).json(action);
-      })
-      .catch(error => {
-        // log error to database
-        console.log(error);
-        res.status(500).json({
-          message: "Error retrieving the actions"
-        });
+  Actions.get()
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the actions"
       });
-  });
+    });
+});
+
+router.post("/", validateProjectId, validateActionPost, (req, res) => {
+  const { notes, description } = req.body;
+  const newAction = {
+    notes,
+    description
+  };
+  Projects.insert(newAction)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `There was an error creating this Action: ${error}`
+      });
+    });
+
+  res.send(newAction);
+});
 
 
 
+//   middleware
 
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid project id" });
+      }
+    })
+    .catch(next);
+}
+
+function validateActionId(req, res, next) {
+  const { id } = req.params;
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid action id" });
+      }
+    })
+    .catch(next);
+}
+
+function validateActionPost(req, res, next) {
+  if (Object.keys(req.body).length) {
+    const { notes, description } = req.body;
+    if (notes) {
+      next();
+    } else if (description) {
+      next();
+    } else {
+      res.status(400).json({ message: "missing required action field" });
+    }
+  } else {
+    res.status(400).json({ message: "missing actiom data" });
+  }
+}
 
 module.exports = router;
